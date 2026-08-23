1bd53192e4f368cb18efe948200f2a981abe3dc0
Gioele-Pgni <300454462+Gioele-Pgni@users.noreply.github.com>
2026-08-23 12:50:37 +0200

    Implement input validation in number_pattern function
    Add input validation for number_pattern function.

--- 02-loops-and-sequences/Build_a_Number_Pattern_Generator.py
+++ 02-loops-and-sequences/Build_a_Number_Pattern_Generator.py
@@ -1,1 +1,11 @@
+def number_pattern(n):
+    if not isinstance(n,int):
+        return "Argument must be an integer value."
+    elif n < 1:
+        return "Argument must be an integer greater than 0."
+    else:
+        lista = []
+        for num in range(n):
+            lista.append(str(num+1))
 
+    return " ".join(lista)
